5f30351389bf9a2bba394da5378e6c0dbc49a7a7
294385049 <ccr606388@163.com>
2016-08-28 21:30:52 +0800

    login-page

--- chatroom/routes/index.js
+++ chatroom/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/index.html');
 });
+
+//暂时没搞数据库，弄个坑爹的文本先记着吧
+
 
 module.exports = router;
